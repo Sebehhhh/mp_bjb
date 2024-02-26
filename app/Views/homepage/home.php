38ce92db573b522243d5d5a39f8e645064057669
Seman <muhammad.seman030801@gmail.com>
2024-02-26 16:15:14 +0800

    fixed owl homepage

--- app/Views/homepage/home.php
+++ app/Views/homepage/home.php
@@ -19,44 +19,47 @@
             <div class="col-md-12 col-lg-7">
                 <h4 class="mb-3 text-light">Welcome To</h4>
                 <h1 class="mb-5 display-3 text-light">Banjarbaru Ramadhan Festival 2024</h1>
-
             </div>
-            <!-- <div class="col-md-12 col-lg-5">
-                <div id="carouselId" class="carousel slide position-relative" data-bs-ride="carousel">
-                    <div class="carousel-inner" role="listbox">
-                        <div class="carousel-item active rounded">
-                            <div class="square-wrapper">
-                                <img src="<?= base_url('asset/img/food.jpg'); ?>" class="img-fluid rounded" alt="First slide">
-                            </div>
-                            <a href="#" class="btn px-4 py-2 text-white rounded">Food</a>
-                        </div>
-                        <div class="carousel-item rounded">
-                            <div class="square-wrapper">
-                                <img src="<?= base_url('asset/img/drink.jpg'); ?>" class="img-fluid rounded" alt="Second slide">
-                            </div>
-                            <a href="#" class="btn px-4 py-2 text-white rounded">Drink</a>
-                        </div>
-                    </div>
-
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselId" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselId" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
-                </div>
-            </div> -->
         </div>
     </div>
 </div>
 <!-- Hero End -->
 
+<!-- Food Start-->
+<div class="container-fluid vesitable py-1">
+    <div class="container py-1">
+        <div class="d-flex justify-content-between align-items-center mb-3">
+            <h3 class="mb-0"><b>Exhebitor: Pasar Wadai Juara</b></h3>
+            <a href="<?= base_url('/food'); ?>" class="btn btn-primary">View All</a> <!-- Tautan View All -->
+        </div>
+        <div class="owl-carousel vegetable-carousel justify-content-center">
+            <?php for ($i = 0; $i < 8; $i++) { ?>
+                <div class="item">
+                    <a href="<?= base_url('/food'); ?>" class="text-decoration-none">
+                        <div class="border border-primary rounded position-relative vesitable-item">
+                            <div class="vesitable-img square-menu">
+                                <img src="<?= base_url('asset/img/food.jpg'); ?>" alt="">
+                            </div>
+                            <div class="text-white bg-primary px-3 py-1 rounded position-absolute" style="top: 10px; right: 10px;">News</div>
+                            <div class="p-4 rounded-bottom">
+                                <h4><b class="text-light">Parsely</b></h4>
+                                <div class="d-flex justify-content-between flex-lg-wrap">
+                                    <p class="mb-0 text-light">Author: Wilda Salsabila</p>
+                                    <p class="mb-0 mt-0 text-light">Date: 25 Februari 2024</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+</div>
+<!-- Food End -->
 <!-- News Start-->
 <div class="container-fluid vesitable py-1">
     <div class="container py-1">
-        <h1 class="mb-0">News</h1>
+        <h3 class="mb-5"><b>Berita</b></h3>
         <div class="owl-carousel vegetable-carousel justify-content-center">
             <?php for ($i = 0; $i < 8; $i++) { ?>
                 <div class="item">
